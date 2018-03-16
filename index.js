6601a7cc5035c1881cbbb72f397508b11c8863fd
Jarrod Blanton <jarrodblanton13@gmail.com>
2018-03-16 09:30:19 -0400

    Mongoose setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+/** ROUTES */
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen('8000', () => console.log('Listening on port 8000'));
