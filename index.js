1a12ee7eb2e951a0d820c9afe9e4106f6cd01762
Jarrod Blanton <jarrodblanton13@gmail.com>
2018-03-20 14:17:44 -0400

    CREATE route finished

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const todoRoutes = require('./routes/todos');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 /** ROUTES */
 app.get('/', (req, res) => res.send('Hello World!'));
 
 // Require all other routes
 app.use('/api/todos', todoRoutes);
+
 
 app.listen('8000', () => console.log('Listening on port 8000'));
 
--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const bodyParser = require('body-parser');
 
+/** /api/todos */
+
+// INDEX
 router.get('/', function(req, res) {
     db.Todo.find()
         .then(todos => {
@@ -12,5 +16,20 @@ router.get('/', function(req, res) {
         })
     res.send('Hello from TODOS Routes');
 });
+
+// CREATE a TODO
+router.post('/', function(req, res) {
+    db.Todo.create(req.body)
+        .then(newtodo => {
+            res.status(201).json(newtodo);
+        }).catch(err => {
+            res.send(err);
+        });
+});
+
+// UPDATE a TODO
+router.put('/:id', function(req, res) {
+    // Get id and match in DB -> make changes -> save
+})
 
 module.exports = router;
